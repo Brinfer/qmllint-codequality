c8bbeb19c2597a0180f7228350679c8ae593988a
Brinfer <brinfer@proton.me>
2024-07-27 12:40:11 +0000

    Add tests report
    Generate coverage and test report for GitLab
    Use workflow instead of multiple rules
    Allow the use of latest version of `qmllint`
    
    Related to #18
    
    See merge request !5

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,6 +3,8 @@
 import logging
 import os
 import re
+import pathlib
+import shutil
 import subprocess
 import sys
 from packaging.version import Version, InvalidVersion
@@ -11,7 +13,7 @@ sys.path.append("../qmllint_codequality")  # Add the package to the sys path
 
 logger = logging.getLogger("qmllint_codequality.test")
 
-QML_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "qml")
+QML_DIR = pathlib.Path(pathlib.Path(__file__).parent, "qml")
 """Path to the directory containing all the QML files."""
 
 __REGEX_CAPTURE_VERSION = re.compile(r"(\d+\.\d+)\.?")
@@ -20,11 +22,11 @@ __REGEX_CAPTURE_VERSION = re.compile(r"(\d+\.\d+)\.?")
 Capture only the major and the minor value of the version, ignore the patch part.
 """
 
-__QMLLINT_MINIMAL_VERSION = Version("6.4.0")
+__QMLLINT_MINIMAL_VERSION = Version("6")
 """Minimal version of qmllint supported."""
 
 
-def get_all_qml_file() -> list[str]:
+def get_all_qml_file() -> list[pathlib.PurePath]:
     """Get the absolute path of all the QML files.
 
     Search recursively the QML file in the QML_DIR folder.
@@ -39,14 +41,14 @@ def get_all_qml_file() -> list[str]:
     for dirpath, _, filenames in os.walk(QML_DIR):
         for filename in filenames:
             if filename.endswith(".qml"):
-                qml_files.append(os.path.join(dirpath, filename))
+                qml_files.append(pathlib.PurePath(dirpath, filename))
                 logger.debug("Find QML file '%s'", qml_files[-1])
 
     logger.info("Find %d QML files", len(qml_files))
     return qml_files
 
 
-def run_qmllint(report_file: str, qml_files: list[str]) -> None:
+def run_qmllint(report_file: pathlib.Path, qml_files: list[pathlib.PurePath]) -> None:
     """Execute the qmllint command line.
 
     Remove the previous ``report_file`` if exist.
@@ -55,24 +57,29 @@ def run_qmllint(report_file: str, qml_files: list[str]) -> None:
     :type report_file: str
     :param qml_files: The list of QML file.
     :type qml_files: list[str]
-    :raises FileNotFoundError: Failed to generate the ``report_file``
+    :raises ExecError: Failed to find the command ``qmllint``
     :raises InvalidVersion: Current version of qmllint is not supported
+    :raises FileNotFoundError: Failed to generate the ``report_file``
     """
+    # Ensure that qmllint is installed
+    if shutil.which("qmllint"):
+        # Ensure that the qmllint version is correct
+        with subprocess.Popen("qmllint --version", stdout=subprocess.PIPE, shell=True, text=True) as qmllint_version:
+            qmllint_version.wait()
+            assert qmllint_version.stdout
+
+            if version_match := __REGEX_CAPTURE_VERSION.search(qmllint_version.stdout.read()):
+                version = Version(version_match.group(1))
+                logger.info("Current version of qmllint: '%s'", version)
+
+                if version < __QMLLINT_MINIMAL_VERSION:
+                    raise InvalidVersion(f"Minimal version requirement not fulfilled (>={__QMLLINT_MINIMAL_VERSION})")
+            else:
+                logger.warning("qmllint's version not found. Continue anyway ...")
+    else:
+        raise shutil.ExecError("qmllint not found")
+
     logger.info("Run the qmllint tool, and save result in %s", report_file)
-
-    # Ensure that the qmllint version is correct
-    with subprocess.Popen("qmllint --version", stdout=subprocess.PIPE, shell=True, text=True) as qmllint_version:
-        qmllint_version.wait()
-        assert qmllint_version.stdout
-
-        if (version_match := __REGEX_CAPTURE_VERSION.search(qmllint_version.stdout.read())):
-            version = Version(version_match.group(1))
-            logger.info("Current version of qmllint: '%s'", version)
-
-            if version < __QMLLINT_MINIMAL_VERSION:
-                raise InvalidVersion(f"Minimal version requirement not fulfilled (>={__QMLLINT_MINIMAL_VERSION})")
-        else:
-            logger.warning("qmllint's version not found. Continue anyway ...")
 
     subprocess.run(
         " ".join(
@@ -80,31 +87,15 @@ def run_qmllint(report_file: str, qml_files: list[str]) -> None:
                 "qmllint",
                 "--dry-run",
                 f"--json '{report_file}'",
-                "--alias warning",
-                "--compiler warning",
-                "--controls-sanity warning",
-                "--deferred-property-id warning",
-                "--deprecated warning",
-                "--import warning",
-                "--inheritance-cycle warning",
-                "--multiline-strings warning",
-                "--multiple-attached-objects warning",
-                "--property warning",
-                "--required warning",
-                "--signal warning",
-                "--type warning",
-                "--unqualified warning",
-                "--unused-imports warning",
-                "--with warning",
                 *[f"'{file}'" for file in qml_files],
             ]
         ),
         check=False,
         shell=True,
-        stdout=subprocess.DEVNULL,
-        stderr=subprocess.DEVNULL,
+        # stdout=subprocess.DEVNULL,
+        # stderr=subprocess.DEVNULL,
     )
 
     # Ensure that the report has been generated
-    if not os.path.isfile(report_file):
+    if not report_file.is_file():
         raise FileNotFoundError(f"'{report_file}' has not been generated")
--- tests/conftest.py
+++ tests/conftest.py
@@ -5,12 +5,13 @@
 """
 
 import pytest
+import pathlib
 
 from tests import get_all_qml_file, logger, run_qmllint
 
 
 @pytest.fixture(scope="session")
-def qmllint_report(tmp_path_factory: pytest.TempPathFactory) -> str:
+def qmllint_report(tmp_path_factory: pytest.TempPathFactory) -> pathlib.Path:
     """Execute qmllint on all QML files.
 
     The report is stored in a temporary file.
@@ -24,7 +25,8 @@ def qmllint_report(tmp_path_factory: pytest.TempPathFactory) -> str:
        - ``tests.get_all_qml_file``
        - ``tests.run_qmllint``
     """
-    report = str(tmp_path_factory.mktemp("report").joinpath("qmllint_report.json"))
+    report = tmp_path_factory.mktemp("report").joinpath("qmllint_report.json")
+
     run_qmllint(report, get_all_qml_file())
 
     logger.debug("qmllint report file written in '%s'", report)
